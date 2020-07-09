1020ab3faa18671c093d9600fdf1e0e8b064e41b
marshallhelenm <marshallhelenm@gmail.com>
2020-07-09 12:58:54 -0700

    made some initial functions to help with saving items to lists

---
+++ src/list logic/ListLogic.js
@@ -0,0 +1,25 @@
+let watchList = {}
+
+let getList = (listName) => { // retrieves a list from localStorage by its name and turns it into an object
+    return JSON.parse(localStorage.getItem(listName))
+}
+
+let setList = (listName, list) => { // sets/updates a list in localStorage
+    localStorage.setItem(listName, JSON.stringify(list))
+}
+
+let addToList = (movie, listName) => { //adds a movie to a list
+    let list = getList(listName)
+    if (!list[movie]){
+        list[movie] = movie
+    }
+    setList(listName, list)
+}
+
+let removeFromList = (movie, listName) => {
+    let list = getList(listName)
+    if (!list[movie]){
+        list[movie] = movie
+    }
+    setList(listName, list)
+}
