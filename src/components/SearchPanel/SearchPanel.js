3cdeaea09069c3189ccbac29dd181df42925f20b
marshallhelenm <marshallhelenm@gmail.com>
2020-07-14 13:38:52 -0700

    made several bare bones components

---
+++ src/components/SearchPanel/SearchPanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// the portion of the page containing various searching components.
+
+function NavBar() {
+  return (
+    <div>
+        {/* filter box, search bar, etc. */}
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// the portion of the page containing all the search results.
+
+function SearchResults() {
+  const generateResultCards = (results) => {
+    // a function to generate a SearchResult component for each result
+  };
+  return (
+    <div>
+      <h1>Results:</h1>
+      <div>{/* {generateResultCards(results)} */}</div>
+    </div>
+  );
+}
+
+export default SearchResults;
