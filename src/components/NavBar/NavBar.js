1d820d6179554b3387f788d81c077c9fba131843
Brian Francoeur <francoeurbj@gmail.com>
2020-07-16 10:29:59 -0400

    resolve merge conflicts

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// navbar along top of page
+
+function NavBar() {
+  return (
+    <div>
+        {/* logo, login/signup, etc. */}
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// a single search result. receives a single movie as prop
+
+function SearchResult(movie) {
+
+  return (
+    <div>
+        {/* image */}
+        <div>
+            {/* title */}
+            {/* text */}
+            {/* etc */}
+        </div>
+    </div>
+  );
+}
+
+export default SearchResult;
