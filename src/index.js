cf45ce914f5279fc49e5705f79e78e9d7d6a5a9c
Brian Francoeur <francoeurbj@gmail.com>
2020-07-16 12:44:06 -0400

    delete PROJECT.md, resolve merge conflicts in index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 import * as admin from 'firebase-admin';
-=======
->>>>>>> c58ea119bd971541874fefda776ce4464192a207
-=======
->>>>>>> c58ea119bd971541874fefda776ce4464192a207
+
 
 ReactDOM.render(
   <React.StrictMode>
