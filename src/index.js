a024149608cf786f52ad74501607fb7f32358c86
Brian Francoeur <francoeurbj@gmail.com>
2020-07-16 10:40:12 -0400

    push changes to PROJECT.md and, package json files, and src/index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+<<<<<<< HEAD
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 import * as admin from 'firebase-admin';
+=======
+>>>>>>> c58ea119bd971541874fefda776ce4464192a207
 
 ReactDOM.render(
   <React.StrictMode>
