46c76c3d2cd156720f91c381294491e905594d9a
Brian Francoeur <francoeurbj@gmail.com>
2020-07-16 10:23:03 -0400

    integrate firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+import * as admin from 'firebase-admin';
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
