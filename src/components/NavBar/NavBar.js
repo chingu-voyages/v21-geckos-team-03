2c8dc065dcea771ac9f498dfee63be141f800119
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-07-17 13:37:53 -0700

    more clean-up

--- src/components/NavBar/NavBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// navbar along top of page
-
-function NavBar() {
-  return <div>{/* logo, login/signup, etc. */}</div>;
-}
-
-export default NavBar;
--- src/components/SearchPanel/SearchPanel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// the portion of the page containing various searching components.
-
-function NavBar() {
-  return <div>{/* filter box, search bar, etc. */}</div>;
-}
-
-export default NavBar;
