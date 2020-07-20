53fe5b61da243ba8f06cbad73903108bf0c8ff96
Brian Francoeur <francoeurbj@gmail.com>
2020-07-20 08:29:34 -0400

    create local firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAoHYf5Y18V0KtgA4KhsVmq097I7HpFDIQ',
+  authDomain: 'geckos-03-movie-app.firebaseapp.com',
+  databaseURL: 'https://geckos-03-movie-app.firebaseio.com',
+  projectId: 'geckos-03-movie-app',
+  storageBucket: 'geckos-03-movie-app.appspot.com',
+  messagingSenderId: '309894013278',
+  appId: '1:309894013278:web:963efac864c127013eefcf',
+  measurementId: 'G-0XTZXCRDSS',
+};
+
+export default firebaseConfig;
