75062576cb3fd04a898a9ffcdae831c5803796e7
marshallhelenm <marshallhelenm@gmail.com>
2020-07-22 10:12:18 -0700

    working on saveMovie button on moviecard

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,9 +1,38 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 
 function MovieCard(props) {
+  const [activeList, setActiveList] = useState([]);
   console.log(props);
   const { movie } = props;
+
+  const userLists = [{ name: 'action movies' }, { name: 'movie night' }];
+
+  const generateLists = () => {
+    if (!userLists) {
+      return [];
+    }
+    console.log('generating lists for dropdown', userLists);
+    const options = userLists.map((list) => {
+      console.log(list);
+      return (
+        <option key={list.name} value={list.name}>
+          {list.name}
+        </option>
+      );
+    });
+    return options;
+  };
+
+  const saveMovie = () => {
+    // firebase logic needs to go here
+    console.log('saving movie to list', movie, activeList);
+  };
+
+  const handleChange = (e) => {
+    setActiveList(e.target.value);
+  };
+
   return (
     <div className="card">
       <img
@@ -26,6 +55,14 @@ function MovieCard(props) {
           </small>
         </p>
         <p className="card--desc">{movie.overview}</p>
+      </div>
+      <div>
+        <select name="list" id="list" onChange={handleChange}>
+          {generateLists()}
+        </select>
+        <button type="submit" onClick={saveMovie}>
+          Save
+        </button>
       </div>
     </div>
   );
