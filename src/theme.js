678615bf050b5e96e1b1ad24abb2eb6c568d5f7e
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-07-22 17:05:36 -0700

    add responsive nav

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,15 @@
+/* eslint-disable prefer-destructuring */
 import { theme } from '@chakra-ui/core';
+
+const breakpoints = ['360px', '768px', '1024px', '1440px'];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
 
 export const customTheme = {
   ...theme,
+  breakpoints,
   fonts: {
     heading: "'Spartan', sans-serif",
     body: "'Spartan', sans-serif",
