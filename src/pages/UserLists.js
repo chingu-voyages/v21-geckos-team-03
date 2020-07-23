7062374320c732090222b74f90236a54e93011b4
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-07-23 11:51:22 -0700

    add fetch lists from db and put in state effect in UserLists

--- src/pages/UserLists.js
+++ src/pages/UserLists.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import CreateList from '../components/CreateList';
+import { FirebaseContext } from '../firebase';
 
 // import PropTypes from 'prop-types';
 
 const UserLists = (props) => {
+  const { user, firebase } = useContext(FirebaseContext);
+  const [lists, setLists] = useState([]);
+  console.log(lists);
+
+  useEffect(() => {
+    if (user) {
+      try {
+        firebase.db
+          .doc(`users/${user.uid}`)
+          .collection('lists')
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              setLists((prev) => [...prev, doc.data()]);
+            });
+          });
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+  }, [user, firebase.db]);
+
   return <CreateList />;
 };
 
