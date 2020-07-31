22d62fb43ecef03b36f8ba247dac5b5cbcf490fd
marshallhelenm <marshallhelenm@gmail.com>
2020-07-30 17:26:08 -0700

    delete logic in place, but does not rerender yet

--- src/components/DeleteMovieButton.js
+++ src/components/DeleteMovieButton.js
@@ -1,11 +1,25 @@
 /* eslint-disable react/forbid-prop-types */
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { IconButton } from '@chakra-ui/core';
+import { FirebaseContext } from '../firebase';
 
 function DeleteMovieButton({ movie, list }) {
+  const { user, firebase } = useContext(FirebaseContext);
   const deleteMovie = () => {
-    // deleteMovie
+    firebase.db
+      .doc(`users/${user.uid}`)
+      .collection('lists')
+      .doc(list.id)
+      .collection('movies')
+      .doc(`${movie.id}`)
+      .delete()
+      .then(function () {
+        console.log('Document successfully deleted!');
+      })
+      .catch(function (error) {
+        console.error('Error removing document: ', error);
+      });
   };
 
   return <IconButton color="red" icon="delete" onClick={deleteMovie} />;
