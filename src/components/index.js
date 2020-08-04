741fefb223ac6a683831decef5b9da4b3ffa598f
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-08-03 21:03:56 -0700

    remove: delete movie button, edit listdropdown

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 import AuthForm from './AuthForm';
 import DeleteListModal from './DeleteListModal';
-import DeleteMovieButton from './DeleteMovieButton';
-import EditListDropDown from './EditListDropDown';
 import EditListModal from './EditListModal';
 import Layout from './Layout';
 import ListItem from './ListItem';
@@ -16,8 +14,6 @@ import SimpleBox from './SimpleBox';
 export {
   AuthForm,
   DeleteListModal,
-  DeleteMovieButton,
-  EditListDropDown,
   EditListModal,
   NewListModal,
   Layout,
