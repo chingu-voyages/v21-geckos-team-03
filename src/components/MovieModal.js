55b32b469a67798524f7a22ef948e0042442a607
Colin Gibson <45520511+cgefx@users.noreply.github.com>
2020-08-04 21:06:37 -0700

    refactor: move movieModal to components since it dosent render a route

---
+++ src/components/MovieModal.js
@@ -0,0 +1,64 @@
+// /* eslint-disable react/forbid-prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  useDisclosure,
+} from '@chakra-ui/core';
+import MovieThumb from './MovieThumb';
+import SaveMovieDropDown from './SaveMovieDropDown';
+
+const MovieModal = ({ movie, watchLists, isListItem }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <MovieThumb
+        posterPath={movie.poster_path}
+        movieId={movie.id}
+        clickable
+        onClick={onOpen}
+        small={isListItem}
+      />
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{movie.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {!isListItem && (
+              <SaveMovieDropDown movie={movie} watchLists={watchLists} />
+            )}
+            <MovieThumb
+              posterPath={movie.poster_path}
+              movieId={movie.id}
+              clickable={false}
+              onClick={null}
+            />
+            {movie.overview}
+          </ModalBody>
+
+          {/* <ModalFooter></ModalFooter> */}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+MovieModal.propTypes = {
+  movie: PropTypes.object.isRequired,
+  watchLists: PropTypes.arrayOf(PropTypes.object).isRequired,
+  isListItem: PropTypes.bool,
+};
+
+MovieModal.defaultProps = {
+  isListItem: false,
+};
+
+export default MovieModal;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import Forgot from './Forgot';
 import Home from './Home';
 import Login from './Login';
-import MovieModal from './MovieModal';
 import WatchList from './WatchList';
 import WatchLists from './WatchLists';
 
-export { Home, Forgot, Login, MovieModal, WatchList, WatchLists };
+export { Home, Forgot, Login, WatchList, WatchLists };
