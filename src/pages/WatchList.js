bbf238f29333c86954c93c21078f7caacd1a7e1c
Colin Gibson <45520511+cgefx@users.noreply.github.com>
2020-08-04 21:05:35 -0700

    fix: watchList tab order

--- src/pages/WatchList.js
+++ src/pages/WatchList.js
@@ -89,15 +89,38 @@ function WatchList() {
           </Box>
         </Flex>
 
-        {/* List Section */}
         {/* Tab Panels */}
-        <Tabs isFitted variant="enclosed">
+        <Tabs defaultIndex={0} isFitted variant="enclosed">
           <TabList mb="1em">
-            <Tab>All</Tab>
             <Tab>Unwatched</Tab>
             <Tab>Watched</Tab>
+            <Tab>All</Tab>
           </TabList>
           <TabPanels>
+            <TabPanel>
+              {/* Unwatched items */}
+              {listMovies
+                .filter((movie) => !movie.watched)
+                .map((unwatchedMovie) => (
+                  <ListItem
+                    key={unwatchedMovie.id}
+                    data={unwatchedMovie}
+                    listDetails={listDetails}
+                  />
+                ))}
+            </TabPanel>
+            {/* {Watched Items} */}
+            <TabPanel>
+              {listMovies
+                .filter((movie) => movie.watched)
+                .map((watchedMovie) => (
+                  <ListItem
+                    key={watchedMovie.id}
+                    data={watchedMovie}
+                    listDetails={listDetails}
+                  />
+                ))}
+            </TabPanel>
             {/* All list Items */}
             <TabPanel>
               {listMovies.map((movie) => (
@@ -107,14 +130,6 @@ function WatchList() {
                   listDetails={listDetails}
                 />
               ))}
-            </TabPanel>
-            {/* Unwatched items */}
-            <TabPanel>
-              <Text>Unwatched goes here...</Text>
-            </TabPanel>
-            {/* {Watched Items} */}
-            <TabPanel>
-              <Text>Watched goes here...</Text>
             </TabPanel>
           </TabPanels>
         </Tabs>
