2000ee7b03849dabed7179b949eda819899f503f
Rayhan1998 <miahrayhan01@gmail.com>
2020-08-08 22:15:09 -0400

    added TMDB logo and Unreel logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/core';
+import { Box, Text, Image, Flex } from '@chakra-ui/core';
+import Film from '../images/film.png';
 
 function Footer() {
   return (
@@ -14,7 +15,22 @@ function Footer() {
       height="400px"
     >
       <Box padding="50px">
-        <Text fontSize="4xl">UnReel</Text>
+        <Flex justify="space-between" align="center">
+          <Flex align="center">
+            <Image src={Film} h={10} paddingRight="10px" />
+            <Text fontSize="4xl">UnReel</Text>
+          </Flex>
+
+          <div>
+            <Text fontSize="4xl" fontWeight="bold">
+              Powered By
+            </Text>
+            <Image
+              src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
+              size="150px"
+            />
+          </div>
+        </Flex>
       </Box>
     </Box>
   );
