1f8807934a13a77d18e470c870a385882df9ac1d
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-08-09 13:46:48 -0700

    refactor: make api logo a const

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, Text, Image, Flex } from '@chakra-ui/core';
 import Film from '../images/film.png';
 import './style.css';
+import { API_LOGO } from '../utils/config';
 
 function Footer() {
   return (
@@ -34,10 +35,7 @@ function Footer() {
             >
               Powered By
             </Text>
-            <Image
-              src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
-              size={['120px', '130', '150px', '150px']}
-            />
+            <Image src={API_LOGO} size={['120px', '130', '150px', '150px']} />
           </div>
         </Flex>
       </Box>
